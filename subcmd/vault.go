c268359c50da50718849bfab9d5ea6949172c6e8
ieee0824 <ieee0824@ezweb.ne.jp>
2017-03-13 19:22:03 +0900

    vault commandの基本ロジック

--- subcmd/vault.go
+++ subcmd/vault.go
@@ -1,10 +1,26 @@
 package subcmd
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+
+	"github.com/ieee0824/thor/vault"
+)
 
 type vaultParam struct {
 	Pass *string
 	Path *string
+}
+
+func (v *vaultParam) Validate() error {
+	if v.Pass == nil {
+		return errors.New("pass is nil")
+	} else if v.Path == nil {
+		return errors.New("path is nil")
+	}
+	return nil
 }
 
 func parseVaultArgs(args []string) (*vaultParam, error) {
@@ -40,9 +56,33 @@ func (c *Vault) Help() string {
 }
 
 func (c *Vault) Run(args []string) int {
+	param, err := parseVaultArgs(args)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := param.Validate(); err != nil {
+		log.Fatalln(err)
+	}
+	bin, err := ioutil.ReadFile(*param.Path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	vaulter := vault.New(bin)
+	if err := vaulter.Encrypt(*param.Pass); err != nil {
+		log.Fatalln(err)
+	}
+	vaultedJSON, err := json.Marshal(vaulter)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := ioutil.WriteFile(*param.Path, vaultedJSON, 0644); err != nil {
+		log.Fatalln(err)
+	}
+
 	return 0
 }
 
-func (c *Deploy) Synopsis() string {
+func (c *Vault) Synopsis() string {
 	return ""
 }
