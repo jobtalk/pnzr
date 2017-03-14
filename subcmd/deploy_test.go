5d0df119c38cd4924d5bcce67cd6e144cd923ee4
shirase_aoi <ieee0824@ezweb.ne.jp>
2017-03-15 01:27:30 +0900

    testdirectoryの追加

---
+++ subcmd/deploy_test.go
@@ -0,0 +1,13 @@
+package subcmd
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	TEST_DIR = os.Getenv("GOPATH") + "/github.com/ieee0824/thor/test"
+)
+
+func TestReadExternalVariablesFromFile(t *testing.T) {
+}
