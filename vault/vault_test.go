ddb77484a9b5ce15b3b0e9f8054b018f5e8ebdd2
ieee0824 <ieee0824@ezweb.ne.jp>
2017-03-15 15:20:28 +0900

    暗号化されたファイルの判定部分のテスト

---
+++ vault/vault_test.go
@@ -0,0 +1,30 @@
+package vault
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var (
+	TEST_DIR = os.Getenv("GOPATH") + "/src/github.com/ieee0824/thor/test"
+)
+
+func TestIsSecret(t *testing.T) {
+	plainTextPath := TEST_DIR + "/vaultTestFiles/plain.json"
+	chipherTextPath := TEST_DIR + "/vaultTestFiles/chipher.json"
+
+	plain, err := ioutil.ReadFile(plainTextPath)
+	if err != nil {
+		t.Error(err)
+	} else if !IsSecret(plain) {
+		t.Errorf("The result is illegal. I want %v, but it is actually %v.", false, IsSecret(plain))
+	}
+
+	chipher, err := ioutil.ReadFile(chipherTextPath)
+	if err != nil {
+		t.Error(err)
+	} else if IsSecret(chipher) {
+		t.Errorf("The result is illegal. I want %v, but it is actually %v.", true, IsSecret(chipher))
+	}
+}
