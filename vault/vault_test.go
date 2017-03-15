552dc0c5cf14f1dc5b4953d85b029f785e22a9b2
ieee0824 <ieee0824@ezweb.ne.jp>
2017-03-15 15:33:35 +0900

    暗号化部分のテスト

--- vault/vault_test.go
+++ vault/vault_test.go
@@ -2,13 +2,29 @@ package vault
 
 import (
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 )
 
 var (
 	TEST_DIR = os.Getenv("GOPATH") + "/src/github.com/ieee0824/thor/test"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
 
 func TestIsSecret(t *testing.T) {
 	plainTextPath := TEST_DIR + "/vaultTestFiles/plain.json"
@@ -26,5 +42,23 @@ func TestIsSecret(t *testing.T) {
 		t.Error(err)
 	} else if !IsSecret(chipher) {
 		t.Errorf("The result is illegal. I want %v, but it is actually %v.", true, IsSecret(chipher))
+	}
+}
+
+func TestEncryption(t *testing.T) {
+	var randomStr = randStringRunes(255)
+	var key = "test"
+	encrypter := NewString(randomStr)
+
+	if err := encrypter.Encrypt(key); err != nil {
+		t.Error(err)
+	} else if string(encrypter.Chipher) == randomStr {
+		t.Errorf("cipher text and plain text is match")
+	}
+
+	if result, err := encrypter.Decrypt(key); err != nil {
+		t.Error(err)
+	} else if string(result) != randomStr {
+		t.Errorf("cipher text and plain text is not match")
 	}
 }
