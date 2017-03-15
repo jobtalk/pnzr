e879f2d4dda46b407d20ff1cc261c582bf7bb199
ieee0824 <ieee0824@ezweb.ne.jp>
2017-03-15 14:36:28 +0900

    add test

--- subcmd/deploy_test.go
+++ subcmd/deploy_test.go
@@ -1,7 +1,20 @@
 package subcmd
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+var (
+	TEST_DIR     = os.Getenv("GOPATH") + "/src/github.com/ieee0824/thor/test"
+	EXTERNSL_DIR = TEST_DIR + "/readExternalVariablesFromFiles"
+)
 
 func TestReadExternalVariablesFromFile(t *testing.T) {
-
+	result, err := readExternalVariablesFromFile(EXTERNSL_DIR)
+	if err != nil {
+		t.Error(err)
+	} else if len(result) != 2 {
+		t.Errorf("The number of elements is invalid. Originally it should be %v, but it is actually %v.", 2, len(result))
+	}
 }
