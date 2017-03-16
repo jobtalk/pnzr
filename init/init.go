f84d3082f5bd150c3ce8a208a54056757d914426
ieee0824 <ieee0824@ezweb.ne.jp>
2017-03-16 14:38:30 +0900

    uiのベース

---
+++ init/init.go
@@ -0,0 +1,131 @@
+package init
+
+func isMultiByteChar(r rune) bool {
+	c := string([]rune{r})
+	return 1 != len(c)
+}
+
+func string2Lines(s string, w int) []*Line {
+	var ret = []*Line{}
+	runes := []rune(s)
+	wCounter := 0
+	var stringBuffer string
+	if w == 0 {
+		return []*Line{NewLine(s)}
+	}
+
+	for _, r := range runes {
+		if isMultiByteChar(r) {
+			if wCounter+2 <= w {
+				wCounter += 2
+				stringBuffer += string([]rune{r})
+			} else {
+				l := NewLine(stringBuffer)
+				ret = append(ret, l)
+				wCounter = 2
+				stringBuffer = string([]rune{r})
+			}
+		} else {
+			if wCounter+1 <= w {
+				wCounter += 1
+				stringBuffer += string([]rune{r})
+			} else {
+				l := NewLine(stringBuffer)
+				ret = append(ret, l)
+				wCounter = 1
+				stringBuffer = string([]rune{r})
+			}
+		}
+	}
+	ret = append(ret, NewLine(stringBuffer))
+	return ret
+}
+
+type Line struct {
+	hasCursor bool
+	S         string
+}
+
+func NewLine(s string) *Line {
+	return &Line{S: s}
+}
+
+func (l Line) String() string {
+	if l.hasCursor {
+		return "> " + l.S
+	}
+	return "  " + l.S
+}
+
+func (l *Line) SetCursor() {
+	l.hasCursor = true
+}
+
+func (l *Line) DeleteCursor() {
+	l.hasCursor = false
+}
+
+func (l *Line) ToggleCursor() {
+	l.hasCursor = true != l.hasCursor
+}
+
+type Item struct {
+	Lines []*Line
+}
+
+func (i Item) String() string {
+	var ret string
+	for _, line := range i.Lines {
+		ret = ret + line.String() + "\n"
+	}
+	return ret
+}
+
+func (i *Item) SetCursor() {
+	i.Lines[0].SetCursor()
+}
+
+func (i *Item) DeleteCursor() {
+	i.Lines[0].DeleteCursor()
+}
+
+func (i *Item) ToggleCursor() {
+	i.Lines[0].ToggleCursor()
+}
+
+func NewItem(s string, w ...int) *Item {
+	if len(w) == 0 {
+		return &Item{
+			[]*Line{NewLine(s)},
+		}
+	}
+
+	return &Item{string2Lines(s, w[0])}
+}
+
+type SelectBox struct {
+	cursorPlace int
+	Items       []*Item
+	MaxWidth    int
+}
+
+func NewSelectBox(elems []string, w ...int) *SelectBox {
+	var items = []*Item{}
+	var ret = &SelectBox{}
+	if len(elems) == 0 {
+		return nil
+	}
+
+	if len(w) == 0 {
+		ret.MaxWidth = 0
+	}
+	ret.MaxWidth = w[0]
+	ret.cursorPlace = 0
+
+	for _, elem := range elems {
+		items = append(items, NewItem(elem, w...))
+	}
+	ret.Items = items
+	ret.Items[0].SetCursor()
+	return ret
+}
---
+++ init/init_test.go
@@ -0,0 +1,85 @@
+package init
+
+import "testing"
+
+func TestIsMultiByteChar(t *testing.T) {
+	if isMultiByteChar('a') {
+		t.Errorf("I wanted %v but it was %v.", false, isMultiByteChar('a'))
+	}
+
+	if !isMultiByteChar('あ') {
+		t.Errorf("I wanted %v but it was %v.", true, isMultiByteChar('あ'))
+	}
+}
+
+func TestString2Lines(t *testing.T) {
+	sampleStringAscii := "0123456789"
+	sampleStringMultiByte := "あいうえお"
+
+	if lines := string2Lines(sampleStringAscii, 0); len(lines) != 1 {
+		t.Error(string2Lines(sampleStringAscii, 0))
+	} else if lines[0].S != "0123456789" {
+		t.Errorf("I wanted %v but it was %v.", "0123456789", lines[0].String())
+	}
+
+	if lines := string2Lines(sampleStringAscii, 5); len(lines) != 2 {
+		t.Error(string2Lines(sampleStringAscii, 5))
+	} else if lines[0].S != "01234" {
+		t.Errorf("I wanted %v but it was %v.", "01234", lines[0].String())
+	} else if lines[1].S != "56789" {
+		t.Errorf("I wanted %v but it was %v.", "56789", lines[1].String())
+	}
+
+	if lines := string2Lines(sampleStringAscii, 4); len(lines) != 3 {
+		t.Error(string2Lines(sampleStringAscii, 5))
+	} else if lines[0].S != "0123" {
+		t.Errorf("I wanted %v but it was %v.", "0123", lines[0].String())
+	} else if lines[1].S != "4567" {
+		t.Errorf("I wanted %v but it was %v.", "4567", lines[1].String())
+	} else if lines[2].S != "89" {
+		t.Errorf("I wanted %v but it was %v.", "89", lines[2].String())
+	}
+
+	if lines := string2Lines(sampleStringMultiByte, 0); len(lines) != 1 {
+		t.Error(string2Lines(sampleStringMultiByte, 0))
+	} else if lines[0].S != "あいうえお" {
+		t.Errorf("I wanted %v but it was %v.", "あいうえお", lines[0].String())
+	}
+
+	if lines := string2Lines(sampleStringMultiByte, 5); len(lines) != 3 {
+		t.Error(string2Lines(sampleStringMultiByte, 5))
+	} else if lines[0].S != "あい" {
+		t.Errorf("I wanted %v but it was %v.", "あい", lines[0].String())
+	} else if lines[1].S != "うえ" {
+		t.Errorf("I wanted %v but it was %v.", "うえ", lines[0].String())
+	} else if lines[2].S != "お" {
+		t.Errorf("I wanted %v but it was %v.", "お", lines[0].String())
+	}
+}
+
+func TestLineString(t *testing.T) {
+	line := NewLine("hoge")
+	if line.String() != "  hoge" {
+		t.Errorf(line.String())
+	}
+
+	line.SetCursor()
+	if line.String() != "> hoge" {
+		t.Errorf(line.String())
+	}
+
+	line.DeleteCursor()
+	if line.String() != "  hoge" {
+		t.Errorf(line.String())
+	}
+
+	line.ToggleCursor()
+	if line.String() != "> hoge" {
+		t.Errorf(line.String())
+	}
+
+	line.ToggleCursor()
+	if line.String() != "  hoge" {
+		t.Errorf(line.String())
+	}
+}
