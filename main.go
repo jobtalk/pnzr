df00b32f7ee1eba5d739628f10903319ed4d3da7
ieee0824 <ieee0824@ezweb.ne.jp>
2017-03-31 19:11:55 +0900

    main関数変更

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
+
+	"github.com/jobtalk/thor/subcmd/deploy"
+	"github.com/jobtalk/thor/subcmd/mkelb"
+	"github.com/jobtalk/thor/subcmd/vault"
+	"github.com/mitchellh/cli"
 )
 
 const (
@@ -14,5 +19,22 @@ func init() {
 }
 
 func main() {
-	fmt.Println("vim-go")
+	c := cli.NewCLI("thor", VERSION)
+	c.Args = os.Args[1:]
+	c.Commands = map[string]cli.CommandFactory{
+		"deploy": func() (cli.Command, error) {
+			return &deploy.Deploy{}, nil
+		},
+		"mkelb": func() (cli.Command, error) {
+			return &mkelb.MkELB{}, nil
+		},
+		"vault": func() (cli.Command, error) {
+			return &vault.Vault{}, nil
+		},
+	}
+	exitCode, err := c.Run()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	os.Exit(exitCode)
 }
