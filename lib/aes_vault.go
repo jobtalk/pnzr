bf19fb088e818675794f2c6c85b8bc7f890ef77c
ieee0824 <ieee0824@ezweb.ne.jp>
2017-04-03 19:47:28 +0900

    kmsで暗号化するための実装

--- lib/aes_vault.go
+++ lib/aes_vault.go
@@ -3,10 +3,10 @@ package lib
 type AES struct {
 }
 
-func (aes *AES) Encrypt(plainText []byte) ([]byte, error) {
+func (a *AES) Encrypt(plainText []byte) ([]byte, error) {
 	return nil, nil
 }
 
-func (aes *AES) Decrypt(cipherText []byte) ([]byte, error) {
+func (a *AES) Decrypt(cipherText []byte) ([]byte, error) {
 	return nil, nil
 }
--- lib/kms_vault.go
+++ lib/kms_vault.go
@@ -1,12 +1,59 @@
 package lib
 
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+)
+
 type KMS struct {
+	KeyID     *string
+	awsConfig *aws.Config
 }
 
-func (aes *KMS) Encrypt(plainText []byte) ([]byte, error) {
-	return nil, nil
+func NewKMS() *KMS {
+	return &KMS{
+		awsConfig: &aws.Config{},
+	}
 }
 
-func (aes *KMS) Decrypt(cipherText []byte) ([]byte, error) {
-	return nil, nil
+func (k *KMS) Encrypt(plainText []byte) ([]byte, error) {
+	svc := kms.New(session.New(), k.awsConfig)
+	params := &kms.EncryptInput{
+		KeyId:     k.KeyID,
+		Plaintext: plainText,
+	}
+	resp, err := svc.Encrypt(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.CiphertextBlob, nil
+}
+
+func (k *KMS) Decrypt(cipherText []byte) ([]byte, error) {
+	svc := kms.New(session.New(), k.awsConfig)
+	params := &kms.DecryptInput{
+		CiphertextBlob: cipherText,
+	}
+	resp, err := svc.Decrypt(params)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Plaintext, nil
+}
+
+func (k *KMS) SetKeyID(keyID string) *KMS {
+	k.KeyID = &keyID
+	return k
+}
+
+func (k *KMS) SetRegion(region string) *KMS {
+	k.awsConfig.Region = &region
+	return k
+}
+
+func (k *KMS) SetAWSConfig(awsConfig *aws.Config) *KMS {
+	k.awsConfig = awsConfig
+	return k
 }
