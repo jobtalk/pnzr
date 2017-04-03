8f55072a777ada03b7b4c1980608676b990948f2
ieee0824 <ieee0824@ezweb.ne.jp>
2017-04-03 19:25:31 +0900

    kmsとaesを実装するためのベース

---
+++ lib/aes_vault.go
@@ -0,0 +1,12 @@
+package lib
+
+type AES struct {
+}
+
+func (aes *AES) Encrypt(plainText []byte) ([]byte, error) {
+	return nil, nil
+}
+
+func (aes *AES) Decrypt(cipherText []byte) ([]byte, error) {
+	return nil, nil
+}
---
+++ lib/kms_vault.go
@@ -0,0 +1,12 @@
+package lib
+
+type KMS struct {
+}
+
+func (aes *KMS) Encrypt(plainText []byte) ([]byte, error) {
+	return nil, nil
+}
+
+func (aes *KMS) Decrypt(cipherText []byte) ([]byte, error) {
+	return nil, nil
+}
