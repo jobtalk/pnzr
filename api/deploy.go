fa3f69709c27e1707f141f77678c4029bdf1d0f2
ieee0824 <ieee0824@ezweb.ne.jp>
2017-04-19 11:24:22 +0900

    nil check修正

--- api/deploy.go
+++ api/deploy.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/jobtalk/thor/lib"
 	"github.com/jobtalk/thor/lib/setting"
 )
@@ -10,14 +11,27 @@ import (
 // 存在するときはアップデートする
 func Deploy(awsConfig *aws.Config, s *setting.Setting) (interface{}, error) {
 	var result = []interface{}{}
-	if s.ECS != nil && s.ELB != nil {
+	if s.ELB != nil {
 		resultMkELB, err := MkELB(awsConfig, s.ELB)
 		if err != nil {
 			return nil, err
 		}
 		result = append(result, resultMkELB)
+		for _, v := range result {
+			if es, ok := v.([]interface{}); ok {
+				for _, e := range es {
+					if tgOutput, ok := e.(*elbv2.CreateTargetGroupOutput); ok {
+						for _, tg := range tgOutput.TargetGroups {
+							lb := s.ECS.Service.LoadBalancers[0]
+							lb.TargetGroupArn = tg.TargetGroupArn
+							s.ECS.Service.LoadBalancers[0] = lb
+						}
+					}
+				}
+			}
+		}
 	}
-	if s.ECS.TaskDefinition != nil {
+	if s.ECS != nil && s.ECS.TaskDefinition != nil {
 		resultTaskDefinition, err := lib.RegisterTaskDefinition(awsConfig, s.ECS.TaskDefinition)
 		if err != nil {
 			return nil, err
