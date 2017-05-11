a9ac9a84bc5e125d982fa0b4fe23ecbf2268bbdc
ieee0824 <ieee0824@ezweb.ne.jp>
2017-05-11 10:33:52 +0900

    設定を生成する部分のテスト

--- lib/conf_test.go
+++ lib/conf_test.go
@@ -1,6 +1,10 @@
 package lib
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+)
 
 func TestIsJson(t *testing.T) {
 	notJSONstr := "this is not json"
@@ -17,5 +21,45 @@ func TestIsJson(t *testing.T) {
 
 	if isJSON(jsonStr) != true {
 		t.Fatalf("\"%v\"であるべきだが\"%v\"だった", true, !true)
+	}
+}
+
+func TestEmbedde(t *testing.T) {
+	dst := new(bytes.Buffer)
+
+	baseConfStr := `
+	{
+		"foo": "bar",
+		"any_val": $val
+	}
+	`
+	embeddedVal := `{"val": {"hoge":"huga"}}`
+
+	resultJSON := `{"foo":"bar","any_val":{"hoge":"huga"}}`
+
+	result, err := Embedde(baseConfStr, embeddedVal)
+	if err != nil {
+		t.Fatal("error: %v", err.Error())
+	}
+	if !isJSON(result) {
+		t.Fatal("%v is not json", result)
+	}
+
+	src := []byte(result)
+	if err := json.Compact(dst, src); err != nil {
+		t.Fatal("error: %v", err.Error())
+	}
+
+	if resultJSON != dst.String() {
+		errString := ""
+		errString += "result: \n"
+		errString += dst.String()
+		errString += "\n\n"
+
+		errString += "answer: \n"
+		errString += resultJSON
+		errString += "\n\n"
+
+		t.Fatal(errString)
 	}
 }
